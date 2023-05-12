80f6865882f80f96811cb4c89b76c806b9fa6e6c
Mike Salcedo <mikesalcedo191@gmail.com>
2023-05-12 08:49:45 -0400

    Update finishing createGrid() function works fine when making the grid now working on color

--- javascript_file.js
+++ javascript_file.js
@@ -1,21 +1,21 @@
 // Trying to make 16*16 grids
 
-// Issue #1 It's only making 2 divs need to figure out what I'm doing wrong.
-// Figured it out the append needs to be inside the for loop.
-
 const gridBox = document.querySelector(".gridBox");
 let secondGridBox;
 let smallGrids;
+const gridDimensions = prompt("what size of a grid do you want");
+// Issue #1 It's only making 2 divs need to figure out what I'm doing wrong.
+// Figured it out the append needs to be inside the for loop.
 
 //Issue #2 Trying make the grids with two for loops one inside another if this doesn't work break them up.
 // Worked perfectly made the first group then the smaller grids inside the secondGridBox
 
-function createSmallGrids() {
-  for (let i = 0; i < 40; i++) {
+function createGrid(gridSize = gridDimensions) {
+  for (let i = 0; i < gridSize; i++) {
     secondGridBox = document.createElement("div");
     secondGridBox.classList.add("secondGridBox");
     gridBox.appendChild(secondGridBox);
-    for (let j = 0; j < 40; j++) {
+    for (let j = 0; j < gridSize; j++) {
       smallGrids = document.createElement("div");
       smallGrids.classList.add("smallGrids");
       secondGridBox.appendChild(smallGrids);
@@ -23,10 +23,21 @@ function createSmallGrids() {
   }
 }
 
-// createSmallGrids();
+createGrid(); // Calling the function works perfectly and makes the grid.
+
+// Trying to make it so my .small grids change color when my mouse goes over them.
+
+function changeSmallGridsColor() {
+  smallGrids.classList.add("newColor");
+}
+
+
+
+
+
 
 /// Second Section using the eraser button to test it out until i figure out how to use the range slider
 
-const eraser = document.querySelector(".eraser");
+// const eraser = document.querySelector(".eraser");
 
-eraser.addEventListener("click", createSmallGrids());
+// eraser.addEventListener("Onclick", createGrid());
