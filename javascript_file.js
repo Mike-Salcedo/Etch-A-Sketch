487fdf7f185c12220fc3cabc1d7a8e2b563f724e
Mike Salcedo <mikesalcedo191@gmail.com>
2023-05-14 17:12:06 -0400

    Update project with solution to changing color on hover with javascript

--- javascript_file.js
+++ javascript_file.js
@@ -1,43 +1,70 @@
 // Trying to make 16*16 grids
 
-const gridBox = document.querySelector(".gridBox");
-let secondGridBox;
-let smallGrids;
-const gridDimensions = prompt("what size of a grid do you want");
 // Issue #1 It's only making 2 divs need to figure out what I'm doing wrong.
-// Figured it out the append needs to be inside the for loop.
 
-//Issue #2 Trying make the grids with two for loops one inside another if this doesn't work break them up.
-// Worked perfectly made the first group then the smaller grids inside the secondGridBox
+// Solution #1 Figured it out the append needs to be inside the for loop.
 
-function createGrid(gridSize = gridDimensions) {
+// Issue #2 Trying make the grids with two for loops one inside another if this doesn't work break them up.
+
+// Solution #2 Worked perfectly made the first group then the smaller grids inside the secondGridBox
+
+// Issue #3 When clicking on the button to make the grid it makes a new one on top of the old one looking weird need to fix the issue so it deletes the old one and keeps the new requested grid layout. //
+
+// Solution #3 Added >>> gridBox.textContent = ""; <<< to update the grid box to blank every time the function is called.
+
+const gridBox = document.querySelector(".gridBox");
+let secondGridBox = document.createElement("div");
+
+let smallGrids = document.createElement("div");
+
+const createGrid = () => {
+  gridBox.textContent = ""; // Deletes the previous grid.
+
+  const gridSize = prompt("what size of a grid do you want");
+
   for (let i = 0; i < gridSize; i++) {
     secondGridBox = document.createElement("div");
     secondGridBox.classList.add("secondGridBox");
     gridBox.appendChild(secondGridBox);
+
     for (let j = 0; j < gridSize; j++) {
       smallGrids = document.createElement("div");
       smallGrids.classList.add("smallGrids");
+
       secondGridBox.appendChild(smallGrids);
     }
   }
+  addColorToDiv();
+};
+
+const createGridButton = document.querySelector(".createGridButton");
+
+// For this to work when clicked on and for it to not fire right away you need to call the function without the () cause if you do createGrid() it's going to ask the user for the grid size without them clicking on the button //.
+
+createGridButton.addEventListener("click", createGrid);
+
+// createGrid(); // Calling the function works perfectly and makes the grid.
+
+// Next Section:
+
+// Trying to add color when the div is hovered over.
+
+// Was able to work it out the addColorToDiv() function needs to be inside of the createGridFunction as a callback though but it's working
+
+function addColorToDiv() {
+  const color = document.querySelectorAll(".smallGrids");
+  color.forEach((btn) =>
+    btn.addEventListener("mouseover", () => btn.classList.add("hover"))
+  );
 }
 
-createGrid(); // Calling the function works perfectly and makes the grid.
+// Clean the grid when clicking on a button but keeping everything else
 
-// Trying to make it so my .small grids change color when my mouse goes over them.
+const deleteGridButton = document.querySelector(".deleteGridButton");
 
-function changeSmallGridsColor() {
-  smallGrids.classList.add("newColor");
+function clearGrid() {
+  const deleter = document.querySelectorAll(".smallGrids");
+  deleter.forEach(deleter.classList.remove(".hover"));
 }
 
-
-
-
-
-
-/// Second Section using the eraser button to test it out until i figure out how to use the range slider
-
-// const eraser = document.querySelector(".eraser");
-
-// eraser.addEventListener("Onclick", createGrid());
+deleteGridButton.addEventListener("click", clearGrid);
